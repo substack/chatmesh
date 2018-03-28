d48b7377f75cba53fd18917cfe5c8f0c48a267ea
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-03-28 22:59:07 +0200

    swarming logic factored out

--- chat.js
+++ chat.js
@@ -5,24 +5,12 @@ var split = require('split2')
 var to = require('to2')
 var pump = require('pump')
 
-var hyperdb = require('hyperdb')
-var addr = /^dat:/.test(argv._[0])
-  ? Buffer(argv._[0].replace(/^dat:\/*/,''),'hex')
-  : null
-if (addr) {
-  var db = hyperdb(argv.d, addr, { valueEncoding: 'utf8' })
-} else {
-  var db = hyperdb(argv.d, { valueEncoding: 'utf8' })
-}
-db.ready(function () {
-  if (!addr) {
-    addr = db.key
-    console.log('dat://' + addr.toString('hex'))
-  }
-  onready()
-})
+var strftime = require('strftime')
+var randomBytes = require('crypto').randomBytes
 
-function onready () {
+open(argv._[0], argv.d, function (err, addr, db) {
+  if (!argv._[0]) console.log('dat://' + db.key.toString('hex'))
+
   pump(db.createHistoryStream(), to.obj(function (row, enc, next) {
     if (row.value) console.log(row.key, row.value)
     next()
@@ -32,46 +20,30 @@ function onready () {
       if (row.value) console.log(row.key, row.value)
     }))
   })
+  require('./swarm.js')(addr, db)
 
-  var discovery = require('discovery-swarm')
-  var swarmDefaults = require('datland-swarm-defaults')
-  console.log('ID',db.local.key)
-  var sw = discovery(swarmDefaults({
-    id: db.local.key,
-    stream: function (peer) {
-      var s = db.replicate({
-        live: true,
-        userData: db.local.key
-      })
-      s.on('error', console.log)
-      return s
-    }
-  }))
-  sw.join(addr.toString('hex'))
-  sw.on('connection', function (peer) {
-    if (!peer.remoteUserData) return
-    db.authorized(peer.remoteUserData, function (err, auth) {
-      if (err) return console.log(err)
-      //console.log('authorized?', auth, peer.remoteUserData.toString('hex'))
-      if (!auth) db.authorize(peer.remoteUserData, function (err) {
-        if (err) return console.log(err)
-      })
+  pump(process.stdin, split(), to(write))
+  function write (buf, enc, next) {
+    var line = buf.toString()
+    var d = new Date
+    var utcDate = new Date(d.valueOf() + d.getTimezoneOffset()*60*1000)
+    var now = strftime('%F-%T', utcDate).replace(/:/g,'_')
+    var key = 'chat/' + now + '-' + randomBytes(8).toString('hex')
+    db.put(key, line, function (err) {
+      if (err) console.log(err)
+      else next()
     })
-  })
-}
+  }
+})
 
-var strftime = require('strftime')
-var randomBytes = require('crypto').randomBytes
-
-pump(process.stdin, split(), to(write))
-function write (buf, enc, next) {
-  var line = buf.toString()
-  var now = Date.now()/1000
-  var frac = now - Math.floor(now)
-  var now = strftime('%F-%T.' + frac)
-  var key = 'chat/' + now + '-' + randomBytes(8).toString('hex')
-  db.put(key, line, function (err) {
-    if (err) console.log(err)
-    else next()
+function open (href, dbdir, cb) {
+  var hyperdb = require('hyperdb')
+  var addr = /^dat:/.test(href)
+    ? Buffer(href.replace(/^dat:\/*/,''),'hex') : null
+  var db = addr
+    ? hyperdb(dbdir, addr, { valueEncoding: 'utf8' })
+    : hyperdb(dbdir, { valueEncoding: 'utf8' })
+  db.ready(function () {
+    cb(null, addr || db.key, db)
   })
 }
