d4a755d74cfadd6440fe934c616375db6ad75863
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-03-29 12:07:31 +0200

    pretty much works

--- chat.js
+++ chat.js
@@ -7,28 +7,136 @@ var pump = require('pump')
 
 var strftime = require('strftime')
 var randomBytes = require('crypto').randomBytes
+var diff = require('ansi-diff')({
+  width: process.stdout.columns,
+  height: process.stdout.rows
+})
+process.stdout.write('\x1b[2J')
+var screen = require('./screen.js')({
+  columns: process.stdout.columns,
+  rows: process.stdout.rows
+})
+process.stdout.on('resize', function () {
+  process.stdout.write('\x1b[2J')
+  diff.resize({
+    width: process.stdout.columns,
+    height: process.stdout.rows
+  })
+  screen.setSize(process.stdout.columns, process.stdout.rows)
+  render()
+})
+function render () {
+  process.stdout.write(diff.update(screen.render(), {
+    moveTo: [screen.cursor+11,screen.rows-2]
+  }))
+}
+var codes = [
+  { code: Buffer.from([0x03]), name: '^C' },
+  { code: Buffer.from([0x04]), name: '^D' },
+  { code: Buffer.from([0x1b,0x5b,0x48]), name: 'home' },
+  { code: Buffer.from([0x1b,0x5b,0x46]), name: 'end' },
+  { code: Buffer.from([0x1b,0x5b,0x44]), name: 'left' },
+  { code: Buffer.from([0x1b,0x5b,0x43]), name: 'right' },
+  { code: Buffer.from([0x1b,0x5b,0x41]), name: 'up' },
+  { code: Buffer.from([0x1b,0x5b,0x42]), name: 'down' },
+  { code: Buffer.from([0x1b,0x5b,0x33,0x7e]), name: 'delete' },
+  { code: Buffer.from([0x7f]), name: 'backspace' }
+]
 
 open(argv._[0], argv.d, function (err, addr, db) {
-  if (!argv._[0]) console.log('dat://' + db.key.toString('hex'))
-
-  pump(db.createHistoryStream(), to.obj(function (row, enc, next) {
-    if (row.value) console.log(row.key, row.value)
-    next()
-  }))
-  db.on('append', function (node) {
-    pump(db.createHistoryStream({ reverse: true }), to.obj(function (row, enc, next) {
-      if (row.value) console.log(row.key, row.value)
-    }))
-  })
-  require('./swarm.js')(addr, db)
-
-  pump(process.stdin, split(), to(write))
-  function write (buf, enc, next) {
-    var line = buf.toString()
+  if (!argv._[0]) {
     var d = new Date
     var utcDate = new Date(d.valueOf() + d.getTimezoneOffset()*60*1000)
-    var now = strftime('%F-%T', utcDate).replace(/:/g,'_')
-    var key = 'chat/' + now + '-' + randomBytes(8).toString('hex')
+    screen.addLine(utcDate, 'dat://' + db.key.toString('hex'), '')
+    render()
+  }
+  setInterval(render, 1000)
+
+  var seen = {}
+  pump(db.createHistoryStream(), to.obj(
+    function (row, enc, next) {
+      writeMsg(row, enc, next)
+      next()
+    },
+    function (next) {
+      db.on('remote-update', onappend)
+      db.on('append', onappend)
+      function onappend (feed) {
+        var h = db.createHistoryStream({ reverse: true })
+        pump(h, to.obj(function (row, enc, next) {
+          writeMsg(row)
+          h.destroy()
+        }))
+      }
+      next()
+    }
+  ))
+  function writeMsg (row) {
+    if (seen[row.key]) return
+    seen[row.key] = true
+    var m
+    if (row.value && (m=/^chat\/([^\/]+)@/.exec(row.key))) {
+      screen.addLine(new Date(m[1]), row.value, row.key)
+      render()
+    }
+  }
+  require('./swarm.js')(addr, db)
+
+  process.stdin.setRawMode(true)
+  var line = ''
+  var buffers = []
+  pump(process.stdin, to(write))
+  function write (buf, enc, next) {
+    for (var i = 0; i < codes.length; i++) {
+      if (!codes[i].code.equals(buf)) continue
+      var name = codes[i].name
+      if (name === '^C') return exit()
+      if (name === 'backspace') {
+        if (screen.cursor > 0) buffers.splice(screen.cursor-1,1)
+        screen.cursor = Math.max(0,screen.cursor-1)
+        screen.setInput(Buffer.concat(buffers).toString())
+        render()
+      }
+      if (name === 'delete') {
+        buffers.splice(screen.cursor,1)
+        //screen.cursor = Math.max(0,screen.cursor+1)
+        screen.setInput(Buffer.concat(buffers).toString())
+        render()
+      }
+      if (name === 'left') {
+        screen.cursor = Math.max(0,screen.cursor-1)
+        render()
+      }
+      if (name === 'right') {
+        screen.cursor = Math.max(0,screen.cursor+1)
+        render()
+      }
+      if (name === 'home') {
+        screen.cursor = 0
+        render()
+      }
+      if (name === 'end') {
+        screen.cursor = screen._input.length
+        render()
+      }
+      return next()
+    }
+    if (buf[0] !== 0x0d) {
+      buffers.splice(screen.cursor,0,buf)
+      screen.setInput(Buffer.concat(buffers).toString())
+      screen.cursor++
+      render()
+      return next()
+    }
+    var line = Buffer.concat(buffers).toString()
+    buffers = []
+    screen.setInput('')
+    screen.cursor = 0
+    render()
+    var d = new Date
+    var utcDate = new Date(d.valueOf() + d.getTimezoneOffset()*60*1000)
+    var now = strftime('%F %T', utcDate)
+    var key = 'chat/' + now + '@' + randomBytes(8).toString('hex')
     db.put(key, line, function (err) {
       if (err) console.log(err)
       else next()
@@ -41,9 +149,11 @@ function open (href, dbdir, cb) {
   var addr = /^dat:/.test(href)
     ? Buffer(href.replace(/^dat:\/*/,''),'hex') : null
   var db = addr
-    ? hyperdb(dbdir, addr, { valueEncoding: 'utf8' })
-    : hyperdb(dbdir, { valueEncoding: 'utf8' })
+    ? hyperdb(dbdir, addr, { sparse: true, valueEncoding: 'utf8' })
+    : hyperdb(dbdir, { sparse: true, valueEncoding: 'utf8' })
   db.ready(function () {
     cb(null, addr || db.key, db)
   })
 }
+
+function exit () { process.exit() }
---
+++ screen.js
@@ -0,0 +1,76 @@
+var strftime = require('strftime')
+var concatMap = require('concat-map')
+
+module.exports = Screen
+
+function Screen (opts) {
+  if (!(this instanceof Screen)) return new Screen(opts)
+  this._lines = []
+  this._tzoffset = new Date().getTimezoneOffset()*60*1000
+  this._input = ''
+  this.columns = opts.columns
+  this.rows = opts.rows
+  this.cursor = 0
+}
+
+Screen.prototype.setSize = function (columns, rows) {
+  this.columns = columns
+  this.rows = rows
+}
+
+Screen.prototype.addLine = function (time, msg, key) {
+  this._lines.push({
+    time: time,
+    message: msg,
+    key: key
+  })
+}
+
+Screen.prototype.setInput = function (msg) {
+  //this.cursor = Math.max(0, this.cursor + (msg.length - this._input.length))
+  this._input = msg
+}
+
+Screen.prototype.render = function () {
+  var self = this
+  var lines = self._lines.sort(function (a, b) {
+    if (a.time < b.time) return -1
+    if (a.time > b.time) return +1
+    return a.key < b.key ? -1 : +1
+  })
+  lines = concatMap(lines, function (line) {
+    var msg = line.message
+    var parts = []
+    var c = self.columns - 12
+    for (var j = 0; j < msg.length; j += c) {
+      parts.push({
+        time: line.time,
+        message: msg.slice(j,j+c),
+        continue: j > 0
+      })
+    }
+    return parts
+  })
+  var N = this.rows-2
+  lines = lines.slice(Math.max(0,lines.length-N+1),lines.length)
+  for (var i = lines.length; i < N-1; i++) {
+    lines.push({ time: null })
+  }
+  var last = strftime('[%T] ' + this._input)
+  last += Array(self.columns-last.length).fill().join(' ')
+  return '' //'\x1b[1G\x1b[1;m'
+    + lines.map(function (line) {
+      var str = ''
+      if (line.continue) {
+        str = Array(12).fill().join(' ') + line.message
+      } else if (line.time) {
+        str = strftime('[%T] ',
+          new Date(line.time-self._tzoffset)) + line.message
+      }
+      str += Array(Math.max(0,self.columns-str.length)).fill().join(' ')
+      return str
+    }).join('\n') + '\n'
+    + Array(self.columns).fill().join('-') + '\n'
+    + last
+    //+ '\x1b[' + (12+this.cursor) + 'G'
+}
