96e864fbd55bb7dfe5be3c6ed96ac64fe9a54c9b
karissa <krmckelv@gmail.com>
2018-05-12 15:46:02 -0500

    dont call callback for bot

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 var Mesh = require('./mesh')
 var Swarm = require('./swarm')
 
-module.exports = function (argv, cb) {
+module.exports = function (argv) {
   if (!argv._[0]) return console.error('Key required.')
   var mesh = Mesh(argv.d, argv._[0], {username: argv.u || 'bot'})
 
   mesh.db.ready(function (err) {
     if (err) throw err
     mesh.swarm = Swarm(mesh)
-    cb(mesh)
   })
 }
